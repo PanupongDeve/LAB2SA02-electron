4bd3ff38ca3e287c02dba1797a5442a77061c30a
panupongdeve <5635512110@psu.ac.th>
2018-09-11 22:08:35 +0700

    update profile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow;
 app.on('ready',function(){
-	mainWindow = new BrowserWindow({width: 1024, height: 768,backgroundColor: '#fff'});
+	mainWindow = new BrowserWindow({width: 1024, height: 768,backgroundColor: '#FF9999'});
 	//mainWindow.loadURL('https://github.com');
 	 mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
